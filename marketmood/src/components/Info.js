25482d5ac53abf0712f1738736131173ac322d9f
Emma Huang <emmahuang.ty@gmail.com>
2022-09-18 05:14:30 -0400

    updated home

---
+++ marketmood/src/components/Info.js
@@ -0,0 +1,24 @@
+function Info() {
+    return (
+        <div class="info">
+            <div class="info1">
+                <h2>Market<br/>Attitude<br/>Tracker</h2>
+                <p>Providing one-click access to summarized data on stock outlook and brand perception</p>
+            </div>
+            <div>
+                <h1>WHO WE ARE</h1>
+                <p>Real-time updated info bank of relevant media articles &#38; publications to help you make trading decisions</p>
+            </div>
+            <div>
+                <h3>Quick &#38; easy</h3>
+                <p>Search query towards a concise yet comprehensive overview of specific publicly traded companies</p>
+            </div>
+            <div>
+                <h2>Skip the read</h2>
+                <p>Natural language processing algorithms to gauge public perception towards your stocks</p>
+            </div>
+        </div>
+    )
+}
+
+export default Info;
