a0fb4e949cf89f4107ac622f45d97b0af5c9bf6b
Fan-Yang <fan.yang284@gmail.com>
2022-09-18 04:29:38 -0400

    install dependencies and create axios call

--- marketmood/src/components/Search.js
+++ marketmood/src/components/Search.js
@@ -1,17 +1,33 @@
 import logo from "../assets/Logo.png";
+import {useState} from 'react';
+import axios from 'axios';
 
-function Search() {
-    const [query, setQuery] = setState("")
-    const handleSubmit = () => {
+const Search = () => {
+    const [query, setQuery] = useState("")
+    const [displayStocks, setDisplayStocks] = useState(null);
 
+    const handleChange = (event) => {
+        setQuery(event.target.value);
+    }
+
+    const handleSubmit = async() => {
+        getStocks(query);
     } 
+
+    const getStocks = async (ticker) => {
+        axios.get('http://127.0.0.0:8000/stock/ticker/' + ticker)
+        .then(resp => {
+            console.log(resp.data);
+            setDisplayStocks(resp.data)
+        })
+        .catch(error => console.log(error))
+    }
+
     return (
         <div class="search">
-            <form>
             <img src={logo} class="headerLogo"/>
-            <input type="text" id="searchBar" name="searchInput" placeholder="Search Company by Ticker"/>
+            <input type="text" id="searchBar" value={query} onChange={handleChange} name="searchInput" placeholder="Search Company by Ticker"/>
             <button onclick={handleSubmit} id="searchSubmit">Search</button>
-        </form>
         </div>
     )
 }
